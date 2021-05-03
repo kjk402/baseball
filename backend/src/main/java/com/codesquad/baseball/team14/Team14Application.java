6e6858c07edcdff0edb547a03b91920b2d0cef45
JoonKi-Kim <joonki402@gmail.com>
2021-05-03 15:35:00 +0900

    feat: init spring boot

---
+++ backend/src/main/java/com/codesquad/baseball/team14/Team14Application.java
@@ -0,0 +1,13 @@
+package com.codesquad.baseball.team14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team14Application.class, args);
+	}
+
+}
